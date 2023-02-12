c5177de62504c15eb8ed139ade11817dfedeb757
saibhandari1999 <saibhandari1999@gmail.com>
2023-02-12 00:23:03 -0700

    multithread

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { Worker } = require("worker_threads");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/non-blocking/", (req, res) => {
+    res.status(200).send("This page is non-blocking");
+  });
+  
+app.get("/blocking", async (req, res) => {
+    const worker = new Worker("./worker.js");
+    worker.on("message", (data) => {
+      res.status(200).send(`result is ${data}`);
+    });
+    worker.on("error", (msg) => {
+      res.status(404).send(`An error occurred: ${msg}`);
+    });
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
